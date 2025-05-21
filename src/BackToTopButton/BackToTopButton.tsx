8a24f4df8f318b382b8bcf5f4ec2b5c78752f029
Haolin Yang <haolinyang95@gmail.com>
2025-05-21 01:43:47 -0700

    add icon, scss & css transition

--- src/BackToTopButton/BackToTopButton.tsx
+++ src/BackToTopButton/BackToTopButton.tsx
@@ -1,22 +1,22 @@
 import { FC, memo } from 'react';
 import { useAtomValue } from 'jotai';
 import { backToTopButtonAtom, gridAPIAtom } from '../context/pageContextAtom';
-import styles from './BackToTopButton.module.css';
+import styles from './BackToTopButton.module.scss';
+import { ArrowUpOutlined } from '@ant-design/icons';
 
 const BackToTopButton: FC = () => {
   console.log(styles);
   const isBackToTopBtnDisplay = useAtomValue(backToTopButtonAtom);
   const gridAPI = useAtomValue(gridAPIAtom);
-  if (!isBackToTopBtnDisplay) return null;
   return (
     <button
       type="button"
-      className={styles.backToTopBtn}
+      className={`${styles.backToTopBtn} ${isBackToTopBtnDisplay ? styles.shows : ''}`}
       onClick={() => {
         gridAPI?.ensureIndexVisible(0);
       }}
     >
-      &#8679;
+      <ArrowUpOutlined />
     </button>
   );
 };
